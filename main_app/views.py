6b96a91e4e399d6d5b962b84d122fef50a165390
Tricerat0x <alex@automy.io>
2024-03-13 12:04:41 -0400

    lab1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def index(request):
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
+
+finches = [
+  {'name': 'Lolo', 'type': 'Zebra Finch', 'description': 'small birds with strips', 'age': 3},
+  {'name': 'Sachi', 'type': 'American Goldfinch', 'description': 'bright yellow and black plumage', 'age': 2},
+]
