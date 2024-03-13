89b479f81f76b1666f9b1983403bd14423d0656b
Tricerat0x <alex@automy.io>
2024-03-13 15:18:23 -0400

    lab3

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-
-# Create your models here.
+from django.urls import reverse
 
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
-    age = models.IntegerField(max_length=100)
+    age = models.IntegerField()  # Removed max_length argument
 
+    def __str__(self):
+        return f'{self.name} ({self.id})'
 
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # Create your views here.
@@ -20,3 +21,18 @@ def finches_index(request):
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   return render(request, 'finches/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+  success_url = '/finches/{finch_id}'
+
+
+class FinchUpdate(UpdateView):
+  model = Finch
+
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
