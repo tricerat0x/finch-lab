2a3d7f76cdaca05a7fc5638c442c006e6bbfadcd
tricerat0x <151412192+tricerat0x@users.noreply.github.com>
2024-04-17 15:56:47 -0400

    Update models.py
    Part 5 update

--- main_app/models.py
+++ main_app/models.py
@@ -3,23 +3,35 @@ from django.urls import reverse
 
 class Finch(models.Model):
     name = models.CharField(max_length=100)
-    breed = models.CharField(max_length=100, blank=True)  # Optional, based on second model
+    breed = models.CharField(max_length=100, blank=True)  
     description = models.TextField(max_length=250)
-    age = models.IntegerField(default=0)  # Added default as age might not always be provided
+    age = models.IntegerField(default=0) 
+    toys = models.ManyToManyField('Toy', blank=True)
 
     def __str__(self):
         return f'{self.name} ({self.breed}) - Age: {self.age}'
 
     def get_absolute_url(self):
-        return reverse('finches_detail', kwargs={'pk': self.pk})  # Unified URL name
+        return reverse('finches_detail', kwargs={'pk': self.pk}) 
 
+
+class Toy(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.pk})
+        
 class Feeding(models.Model):
     date = models.DateField('feeding date')
     meal = models.CharField(max_length=100)
-    finch = models.ForeignKey(Finch, on_delete=models.CASCADE, related_name='feedings')  # Added related_name for reverse access
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE, related_name='feedings') 
 
     def __str__(self):
         return f"{self.meal} on {self.date.strftime('%Y-%m-%d')}"
 
     class Meta:
-        ordering = ['-date']  # Orders feedings by date, latest first
+        ordering = ['-date']  
